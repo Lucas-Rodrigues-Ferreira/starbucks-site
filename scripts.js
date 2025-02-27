6519c3466fad33808ac419aca602a24c244a98fe
Lucas-Rodrigues-Ferreira <lucarmacio@hotmail.com>
2025-02-27 13:05:41 -0300

    Add files via upload
    Subindo os arquivos do site

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+let imagem = document.querySelector(".imagem-copo")
+let circulo = document.querySelector(".circulo")
+
+function trocaimagem(endereco) {
+
+    imagem.src = endereco
+
+}
+    function trocacor(cor) {
+        circulo.style.background = cor
+
+    }
+
+
